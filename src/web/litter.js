cc4495eaac34a6dbab16142443d3f524679ae13c
Benji Englard <benjienglard@yahoo.com>
2012-06-24 16:54:11 -0400

    Includes user interface based keyword search

--- src/web/litter.js
+++ src/web/litter.js
@@ -46,12 +46,49 @@ function loadHeader() {
   $("<h1/>", {text : 'Litter - Microblog for the LAN'}).appendTo("#subheader");
   var menu = $("<ul/>").appendTo("#subheader");
   menu.append($("<li/>", {text : 'Refresh', click : getState}));
+  var search_bar = $("<textarea/>", {"name" : "search_bar", "cols" : "30", "rows" : 1,
+  	  id : "search", text : "Search by username or keyword"}).appendTo("#subheader");
+  search_bar.css({"resize" : "none"});
+  $("<button/>", {text : "Search", "height" : "50px", click : search}).appendTo("#subheader"); 
 }
 
 function doNothing() {}
 
+function search() {
+  if ($("#search").val() != "") {
+	  $("#postdiv").slideUp();
+	  $("#resultsdiv").html("");
+	  var ob = { "m" : "get" , "limit" : 2147483647} //max int in javascript
+	  $.ajax({type: "POST", url: "/api", dataType: 'json', 
+		data : {'json' : JSON.stringify(ob)}, success: processSearch});
+	}
+}
+
+function processSearch(state) {
+  posts = state['posts'];
+  var query = $("#search").val();
+  createTable();
+  var res_counter = 0
+  for (var i = posts.length-1; i >= 0; i--) {
+    var result = {}
+    result['msg'] = posts[i][0]
+    result['uid'] = posts[i][1]
+    result['txtime'] = posts[i][2]
+    result['rxtime'] = posts[i][3]
+    result['postid'] = posts[i][4]
+    result['perms'] = posts[i][5]
+    result['hashid'] = posts[i][6]
+    if (result.msg.indexOf(query) != -1 || result.uid.indexOf(query) != -1) { 
+    	addResult(result, "search");
+    	res_counter += 1
+    }
+  }
+  if (res_counter == 0) 
+  	alert("No messages with \"" + query + "\" found.")
+}
+
 function loadPost() {
-  var q = $("<h1/>", {text : "What's new?"}).appendTo("#postdiv");
+  var q = $("<h1/>", {id : "news_area", text : "What's new?"}).appendTo("#postdiv");
   q.css({"color" : "gray", "text-align" : "left"});
   var tarea = $("<textarea/>", {"name" : "post", "cols" : "100", "rows" : "3", 
       id : "txt"}).appendTo("#postdiv");
@@ -63,7 +100,7 @@ function loadPost() {
       ).appendTo("#postdiv");
   var par = $("<p/>", { id : "countid", text : '140 characters left'}
       ).appendTo("#postdiv");
-  par.css({"color" : "black", "text-align" : "right", "width" : "600px"});
+  par.css({"color" : "black", "text-align" : "left", "width" : "600px"});
   par.css({"font-size" : "20px"});
 	
   tarea.keypress(messageCount);
@@ -83,6 +120,8 @@ function messageCount() {
 }
 
 function loadResults(state) {
+  $("#news_area").html("What's new, " + state[0][1] + "?");
+
   for (var i = state.length-1; i >= 0; i--) {
     var result = {}
     result['msg'] = state[i][0]
@@ -92,7 +131,7 @@ function loadResults(state) {
     result['postid'] = state[i][4]
     result['perms'] = state[i][5]
     result['hashid'] = state[i][6]
-    addResult(result);
+    addResult(result, "N");
   }
 }
 
@@ -110,9 +149,9 @@ function createTable() {
   ratingcol.appendTo(row);
 }
 
-function addResult(result) {
+function addResult(result, reason) {
   // we check to see if we already have this in our table
-  if ($("body").data(result.hashid) != null) {
+  if ($("body").data(result.hashid) != null && reason != "search") {
     return;
   }
 
@@ -134,11 +173,25 @@ function addResult(result) {
   $("<img/>", {'src' : img_src, 'width' : '40px', 
     'height' : '40px'}).appendTo(imgcol);
 
+
   infocol.append($("<p/>", {text: result.uid, 'class' : 'name',
-    'id' : result.uid, click : doNothing}));
-
-  var msg = updateUrl(result.msg);
-
+    'id' : result.uid}));
+    
+  //Clicking on a username brings up most recent most by that user
+  //by doing a search where keyword = uid
+  $('p.name').click(function() {
+    $("#search").html(result.uid);
+    search();
+  });
+  
+  var msg;
+  
+  var query = $("#search").val()
+  if (query != "Search by username or keyword" && query != "") 
+  	msg = updateUrl(result.msg, query);
+  else
+	msg = updateUrl(result.msg, "");
+	
   infocol.append($("<p/>", {'class' : 'msg'}).html(msg));
   infocol.append($("<p/>", { text: date.toString(), 'class' : 'time'}));
   ratingcol.append($("<span/>", {text: '','class': 'rating'}));
@@ -148,7 +201,7 @@ function addResult(result) {
   $("body").data(result.hashid, result);
 }
 
-function updateUrl(msg) {
+function updateUrl(msg, query) {
   var strArray = msg.split(" ");
   for (var i=0; i < strArray.length; i++) {
     if (strArray[i].search("^http://") != -1) {
@@ -183,6 +236,9 @@ function updateUrl(msg) {
       strArray[i] = "<a target=\"_blank\" href=\"http://" + strArray[i] + "\">" + 
           strArray[i] + "</a>";
     }
+    else if (query != "" && strArray[i].indexOf(query) != -1) {
+    	strArray[i] = "<b><i>" + strArray[i] + "</i></b>";
+    }
   }
   var result = strArray.join(" ");
   return result;
@@ -205,6 +261,7 @@ function doPost(perms) {
   $.ajax({type: "POST", url: "/api", dataType: 'json', 
           data : {'json' : JSON.stringify(ob)} ,
           success: getState});
+  var elem = $("#countid").html("140 characters left");
 }
 
 function doFriendPost() {
